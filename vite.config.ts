22268d14e2026647a4efd0c3ff55c0425e9841c9
sknhshrmn <sakinahshahriman@gmail.com>
2023-05-04 04:28:43 +0800

    add: base, change: icon

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/dictionary-app/",
+  base: "/dictionary-app/",
   server: {
     watch: {
       usePolling: true,
